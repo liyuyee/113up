2f154ff6e64afb346e7af1697ebb00797b9852e4
liyuyee <a112221049@mail.shu.edu.tw>
2024-10-04 10:50:26 +0800

    Add files via upload

---
+++ 113-10-04/SecureRandomDemo.java
@@ -0,0 +1,16 @@
+public class SecureRandomDemo {
+    public static void main(String[] args) {
+        java.security.SecureRandom randomNumber = new  java.security.SecureRandom();
+        while (true) { 
+            var number = randomNumber.nextInt(10);
+            System.out.println(number);
+
+            if(number == 5) {
+             System.err.println("I hit 5....Orz");
+                 break;
+            }
+        }
+    }
+    
+} 
+   
